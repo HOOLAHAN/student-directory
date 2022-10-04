2fb14dc59166341e49ec4e25ad2cdf780a7d2e1c
Iain <iain.hoolahan@gmail.com>
2022-10-04 14:10:03 +0100

    step 9 complete

--- directory.rb
+++ directory.rb
@@ -1,12 +1,29 @@
 def input_students
-  puts "Please enter the names of students"
-  puts "To finish, just hit return twice"
   students = []
+  puts "Please enter the name of first student"
   name = gets.chomp
+  puts "Please enter the cohort of first student"
+  cohort = gets.chomp
+  puts "Please enter the country of birth of first student"
+  country = gets.chomp
+  puts "Please enter the height of first student (in cm)"
+  height = gets.chomp
+  
   while !name.empty? do
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort, country_of_birth: country, height: height}
     puts "Now we have #{students.count} students"
-    name = gets.chomp
+    if name == nil
+      break
+    else
+      puts "Please enter the name of next student"
+      name = gets.chomp
+      puts "Please enter the cohort of next student"
+      cohort = gets.chomp.to_sym
+      puts "Please enter the country of birth of next student"
+      country = gets.chomp
+      puts "Please enter the height of next student"
+      height = gets.chomp
+    end
   end
   students
 end
@@ -20,14 +37,21 @@ def print(students)
   counter = 0
   while counter < students.length
     students.each_with_index() do |student, index|
-    puts "#{index + 1} #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index + 1} #{student[:name]}"
+    puts "cohort: #{student[:cohort]}"
+    puts "country of birth: #{student[:country_of_birth]}"
+    puts "height: #{student[:height]}cm"
     counter += 1
     end
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  if names.count == 1
+    puts "Overall, we have #{names.count} great student"
+  else
+    puts "Overall, we have #{names.count} great students"
+  end
 end
 
 students = input_students
