5beffdf46499cfa0b383529feb7bf0ffeccc3774
Iain <iain.hoolahan@gmail.com>
2022-10-05 11:13:01 +0100

    interactive menu added

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
+@students = []
+# get the user to input list of students
 def input_students
-  students = []
   puts "Please enter the name of first student"
   name = gets.chomp
   if !name.empty?
@@ -12,8 +13,8 @@ def input_students
   end
 
   while !name.empty? do
-    students << {name: name, cohort: cohort, country_of_birth: country, height: height}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort, country_of_birth: country, height: height}
+    puts "Now we have #{@students.count} students"
     if name == nil
       break
     else
@@ -27,18 +28,20 @@ def input_students
       height = gets.chomp
     end
   end
-  students
+  @students
 end
 
+# header 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print(students)
+# print students out ordered by entry number
+def print_students_list
   counter = 0
-  while counter < students.length
-    students.each_with_index() do |student, index|
+  while counter < @students.length
+    @students.each_with_index() do |student, index|
     puts "#{index + 1} #{student[:name]}"
     puts "cohort: #{student[:cohort]}"
     puts "country of birth: #{student[:country_of_birth]}"
@@ -48,7 +51,9 @@ def print(students)
   end
 end
 
+
 def group(students)
+puts "Students grouped by cohort:"
   students_by_cohort = {}
   students.each do |student|
     cohort = student[:cohort]
@@ -57,7 +62,7 @@ def group(students)
     end
     students_by_cohort[cohort].push(student[:name])
   end
-# students_by_cohort grouoed using hash
+# students_by_cohort grouped using hash
   students_by_cohort.each do |k, v|
     puts k
     puts v
@@ -72,8 +77,37 @@ def print_footer(names)
   end
 end
 
-students = input_students
-print_header
-print(students)
-print_footer(students)
-group(students)
+def interactive_menu
+  loop do
+    print_menu # print the menu and ask user what to do
+    process(gets.chomp) # read the input and save it to a variable then do what the user asked
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer(@students)
+  group(@students)  
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students # input the students 
+    when "2"
+      show_students # show the students
+    when "9"
+      exit # terminate the program
+    else
+      puts "I don't know what you meant, please try again."
+  end
+end
+
+interactive_menu
